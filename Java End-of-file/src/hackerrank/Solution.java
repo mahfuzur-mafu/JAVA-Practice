e649be8d597bb469f129687cc10bcc5bda70753f
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-11-03 22:47:07 +0600

    Add files via upload

---
+++ Java End-of-file/src/hackerrank/Solution.java
@@ -0,0 +1,14 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Solution {
+	 public static void main(String[] args) {
+	       Scanner sc=new Scanner(System.in);  
+	        int i = 0;
+	        while (sc.hasNextLine()) {
+	            System.out.println(++i + " " + sc.nextLine());
+	    }
+	    }
+
+}
